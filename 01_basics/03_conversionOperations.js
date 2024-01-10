41181685fc9a6767a3ddb305fdfbeb58b4e44840
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-01-10 11:37:33 +0000

    Basic Concepts

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+// let score ="true"
+// console.log(typeof score);
+// console.log(typeof(score));
+ let valueinNumber = Number(score);
+// console.log(typeof valueinNumber);
+// console.log(typeof (valueinNumber));
+// console.log(valueinNumber);
+// //"33" => 33
+// //"33abc "=> NaN
+// //true = 1 / false = 0
+
+// let isLoggedIn = 1
+ let booleanIsLogin = Boolean(isLoggedIn)
+// console.log(booleanIsLogin);
+// //1 = true ; 0 = false
+// //" "=>false
+// //"megha "=> true
+let someNumber = 89
+let StringNumber= String(someNumber)
+console.log(typeof StringNumber);
+console.log(StringNumber);
