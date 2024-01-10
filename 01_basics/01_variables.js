3b43dc728f554bdfb8780a4e79a8ce7241f99861
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-01-10 11:16:10 +0000

    Basic concepts

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1334
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"//use ni krna h because of issue in block scope and functional scope  
+accountCity ="Jaipur"
+let accountstate;
+
+//accountId=2//not allowed
+accountEmail = "hec@gmail.com";
+accountCity="Jabalpur";
+accountPassword="122";
+
+
+
+console.log(accountId,accountEmail,accountPassword,accountCity,accountstate);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"*use strict"//treat all js code as newer version
+//alert( 5+ 9 ) we r u node js not brower
+console.log(3
+    +3);//code readability should be high
+
+console.log("Megha");
+let name = " Megha Namdeo "
+let age = 20
+let isLoggedIn = true
+let state 
+
+/* Primitive Datatype */
+//number =>2 to power 53
+//bigint
+//string=>" " or ' '
+//boolean => standalone value
+//undefined => 
+//symbol => unique
+
+//object 
+console.log(typeof age);
+console.log(typeof null);//output : object 
+console.log(typeof undefined);//output:undefined
