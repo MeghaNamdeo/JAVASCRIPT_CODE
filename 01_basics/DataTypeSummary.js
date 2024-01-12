3c8547e4ac15029541f430ace12ae15e8239fe3f
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-01-12 15:47:32 +0000

    DataType Summary

---
+++ 01_basics/DataTypeSummary.js
@@ -0,0 +1,37 @@
+//Primitive
+/* 7 types : String 
+            Number
+            Boolean
+            null
+            undefined
+            Symbol
+            BigInt
+
+    */
+
+    const score = 100
+    const Valuescore = 89900.90
+    const isLoggedIn= false
+    const outsideTemp = null
+    let userEmail;
+    const id =Symbol('123');
+    const id2=Symbol('123');
+    console.log(id===id2);
+    
+ //  Reference (Non Primitive)   
+ //Array , Objects, Function 
+ const heros=["shaktiman","naagraj","doga"];
+console.log(heros);
+ let myObj={
+    name:"megha",
+    age:20,
+
+ }  
+ console.log(myObj); 
+ 
+ const myfunc = function(){
+    console.log("Hello world");
+ };
+ myfunc();
+ //typeof is used to define the data type
+ 
