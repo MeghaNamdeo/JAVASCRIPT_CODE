d85e054db91f60927e85ed048747db7162bfb127
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-01-16 05:55:27 +0000

    Object part 2

---
+++ 02_basics/04_object.js
@@ -0,0 +1,48 @@
+//const tinderUser = new Object()
+//or
+const tinderUser={}
+tinderUser.id = "123abc"
+tinderUser.name="Megha"
+tinderUser.isLoggedIn=false;
+console.log(tinderUser);
+const regularUser={
+    email:"abc@gmail.com",
+    fullname:
+    {
+        userFullName :
+        {
+            firstName:"megha",
+            lastName:"namdeo"
+        }
+    }
+}
+console.log(regularUser.fullname.userFullName.firstName);
+const obj1={1:"a",2:"b"}
+
+const obj2={3:"a",4:"b"}
+const obj4={5:"a",6:"b"}
+//const obj3 = {obj1,obj2}  //array type value melegi 
+
+//const obj3=Object.assign({},obj1,obj2,obj4)
+//or using spread
+const obj3 = {...obj1,...obj2,...obj4}
+    console.log(obj3);
+    const users=[{//arrray me object createkrna
+        id : 1 ,
+        email:"google@gmail.com"
+    },
+    {//arrray me object createkrna
+        id : 2 ,
+        email:"microsoft@gmail.com"
+    },
+    {//arrray me object createkrna
+        id : 3 ,
+        email:"chatgpt@gmail.com"
+    },
+]
+    console.log(users[1].email)
+    console.log(tinderUser);
+    console.log(Object.keys(tinderUser));//keys print krta hai 
+    console.log(Object.values(tinderUser));//values print krta hai 
+    console.log(Object.entries(tinderUser));//entries print krta hai
+    console.log(tinderUser.hasOwnProperty('isLogged'));//ye batata h ye property hai ki ni true false me ans deta hai
