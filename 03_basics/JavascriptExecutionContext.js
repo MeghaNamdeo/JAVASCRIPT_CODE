b43e1b3e991606a4afd10cdfa15702745a6f67ed
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-01-20 13:05:46 +0000

    theory

---
+++ 03_basics/JavascriptExecutionContext.js
@@ -0,0 +1,13 @@
+/********* Javascript Execution Context: Jo bhi aapne javascript file banai hai bo run kese hogi ***************/
+ /*
+ 1. Global Execution Context
+ 2. Function Execution Context
+ 3. Eval Execution Context
+
+ {}-> Two phase me run hota hai 
+     1)Memory Creation Phase 
+     2)Execution Phase 
+
+
+
+ 
