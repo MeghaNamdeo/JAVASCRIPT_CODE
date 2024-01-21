b1f22f5f886ebd798505275c094460927b28575c
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-01-21 13:01:09 +0000

    while , do while

---
+++ 05_iterations/while.js
@@ -0,0 +1,27 @@
+//while loop
+/* 
+while(condition)
+{
+
+}
+*/
+let index = 0 
+while(index<=10)
+{
+  console.log(`Value of index is ${index}`);
+  index = index + 2
+}
+let myArr=['A','B','C']
+let arr=0
+while(arr<myArr.length)
+{
+  console.log(`Value is ${myArr[arr]}`);
+  arr=arr+1
+}
+let score =1
+
+do{
+  console.log(`Score is ${score}`);
+score++;
+}
+while(score<=10);
