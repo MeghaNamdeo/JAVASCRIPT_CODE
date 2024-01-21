a9e9635b149b65254c398251675202982a9213d7
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-01-21 11:46:05 +0000

    true

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -28,3 +28,19 @@ if(Object.keys(emptyObj).length===0)
     console.log("Object is empty");
 
 }
+//Nullish Coalescing Operator(??): null undefined 
+let val1 ;
+val1 = 5 ?? 10
+val2 = null ?? 10
+
+val3 = undefined ?? 15
+val4 = null ?? 10??10
+console.log("5 ?? 10  : " , val1);
+console.log('null ?? 10  :  ',null ?? 10);
+console.log('undefined ?? 10  : ',undefined ?? 10);
+console.log('unull ?? 10??10 : ',null ?? 10??10);
+
+//ternary operator 
+/*condition? true:false*/
+const iceTeaPrice = 100
+iceTeaPrice <=80 ? console.log("less than 80"): console.log("more than 80");
