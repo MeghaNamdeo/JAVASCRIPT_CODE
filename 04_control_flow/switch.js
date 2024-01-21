2292e1dec1b0eff41cbb04d7bb05a73fbc0e8a0b
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-01-21 11:08:19 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+/*
+switch(key)
+{
+    case value:
+        break;
+    default:
+        break;
+}*/
+
+const month=3
+
+switch(month)
+{
+    case 1:
+        console.log("January"); 
+        break;
+    case 2:
+        console.log("Febuary"); 
+        break;
+    case 3:
+        console.log("March"); 
+        break;
+    case 4:
+        console.log("April"); 
+        break;
+
+    default:
+        console.log("default case match ");
+        break;//break likhna neccessary ni h default ke baad
+}
+//agar break hata denege to case match se saari value print ho jaygi except default
+
