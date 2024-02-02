99ed431f5a5ea93e8d449523d1a3e50946169c08
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-02-02 22:48:29 +0530

    Update Test.js

--- 01_basics/Test.js
+++ 01_basics/Test.js
@@ -1,1 +1,6 @@
-console.log("Megha Namdeo")
+console.log('Megha Namdeo');
+/* The console.log function in JavaScript is a powerful tool 
+used for debugging and logging information during development.
+ It allows developers to output messages, variables, or any data
+  to the browser's console. This is incredibly useful for inspecting values
+  , tracking the flow of the program, and identifying issues in the code. */
