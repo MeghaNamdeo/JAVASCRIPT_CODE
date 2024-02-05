a04ef6adcbd11dcec37e79c6de4f782390fc9dc8
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-02-05 08:57:20 +0000

    js

---
+++ 18_oops_concept/mathPi.js
@@ -0,0 +1,41 @@
+const descripter = Object.getOwnPropertyDescriptor(Math,"PI")
+console.log(descripter);
+// console.log(Math.PI);
+// Math.PI=5
+// console.log(Math.PI);
+
+/*
+output : {
+  value: 3.141592653589793,
+  writable: false,
+  enumerable: false,
+  configurable: false
+}
+*/
+
+const chai ={
+    name : 'ginger chai',
+    price : 250,
+    isAvailable : true,
+    oderChai:function(){
+        console.log("chai nahi bani");
+    }
+}
+console.log(chai);
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+Object.defineProperty(chai,"name", {
+    
+        
+        //writable: false,
+        enumerable: false,
+       
+ })
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for(let [key,value] of Object.entries(chai))
+{
+    if(typeof value !== 'function')
+    {
+    console.log(`${key}:${value}`);
+}
+}
